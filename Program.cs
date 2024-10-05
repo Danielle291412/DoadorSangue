980c07e2f22846ded0aaa17b0f63da188f75e529
--global <--global>
2024-10-04 22:29:59 -0300

    mostrar se o usuário pode ser ou não ser doador de sangue

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int idade;
+bool podeSerDoador;
+
+Console.Clear();
+Console.WriteLine("Qual é a sua idade?");
+
+idade = Convert.ToInt32(Console.ReadLine ()!);
+podeSerDoador = (idade >= 18) && (idade <= 67);
+
+if (podeSerDoador)
+{
+     Console.WriteLine("Você pode ser doador de sangue.");
+}
+else
+{
+    Console.WriteLine("Você não pode ser doador de sangue.");
+}
